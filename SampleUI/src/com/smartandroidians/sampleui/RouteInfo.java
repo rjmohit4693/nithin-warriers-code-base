242e81714ef5774febb7de4f6e1e940e72d10506
nithin.warier <nithin.warier@4c3b25b9-3935-80af-9f20-e2478c9de2e5>
2011-12-27 16:27:30 +0000

    Adding an UI application which has two textviews when clicked on one textView, a list will appear, when clicked on it again list will disappear...

---
+++ SampleUI/src/com/smartandroidians/sampleui/RouteInfo.java
@@ -0,0 +1,12 @@
+package com.smartandroidians.sampleui;
+
+import android.content.Context;
+import android.widget.ScrollView;
+
+public class RouteInfo extends ScrollView {
+	
+	public RouteInfo(Context context) {
+		super(context);
+	}
+
+}
