0caacb316766b1c4b103b28a709686e11e838848
nithin.warier <nithin.warier@4c3b25b9-3935-80af-9f20-e2478c9de2e5>
2012-07-15 14:11:57 +0000

    Adding a application whicch will show map, with marker icon. Also it will show a transparent panel when clicked on the marker.

---
+++ MapWithMarker/src/com/smartandroidians/mapwithmarker/TransparentPanel.java
@@ -0,0 +1,46 @@
+package com.smartandroidians.mapwithmarker;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class TransparentPanel extends LinearLayout {
+	
+	private Paint innerPaint, borderPaint ;
+
+	public TransparentPanel(Context context) {
+		super(context);
+		init();
+	}
+	
+	public TransparentPanel(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+	
+	private void init() {
+		innerPaint = new Paint();
+		innerPaint.setARGB(225, 75, 75, 75); //gray
+		innerPaint.setAntiAlias(true);
+
+		borderPaint = new Paint();
+		borderPaint.setARGB(255, 255, 255, 255);
+		borderPaint.setAntiAlias(true);
+		borderPaint.setStyle(Style.STROKE);
+		borderPaint.setStrokeWidth(2);
+	}
+	
+	@Override
+    protected void dispatchDraw(Canvas canvas) {
+    	RectF drawRect = new RectF();
+    	drawRect.set(0,0, getMeasuredWidth(), getMeasuredHeight());
+    	canvas.drawRoundRect(drawRect, 5, 5, innerPaint);
+		canvas.drawRoundRect(drawRect, 5, 5, borderPaint);
+		super.dispatchDraw(canvas);
+    }
+
+}
